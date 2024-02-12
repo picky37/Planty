437449848767aa7cd00cddcb3f33cac8d5f73307
picky37 <149702922+picky37@users.noreply.github.com>
2024-02-12 14:36:34 +0100

    Améliorations header

--- app/public/wp-content/themes/blankslate-child/header.php
+++ app/public/wp-content/themes/blankslate-child/header.php
@@ -5,6 +5,9 @@
     <meta charset="<?php bloginfo('charset'); ?>" />
     <meta name="viewport" content="width=device-width" />
     <?php wp_head(); ?>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700&display=swap" rel="stylesheet">
 </head>
 
 <body <?php body_class(); ?>>
@@ -12,7 +15,7 @@
     <div id="wrapper" class="hfeed">
         <header id="header" role="banner">
             <div class="logo">
-                <a href="accueil"><img src="http://planty.local/wp-content/uploads/2024/01/Logo-1.png" alt="Logo de Planty"></a>
+                <a href="accueil"><img src="http://planty.local/wp-content/uploads/2024/02/Logo-2.png" alt="Logo de Planty"></a>
             </div>
             <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
                 <?php wp_nav_menu(array('theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>')); ?>
