5d0382c101b2a347213d007b89f19b06dc901136
picky37 <149702922+picky37@users.noreply.github.com>
2024-02-29 09:44:54 +0100

    Modif formulaire commane

--- app/public/wp-content/themes/blankslate-child/functions.php
+++ app/public/wp-content/themes/blankslate-child/functions.php
@@ -21,4 +21,29 @@ if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
 endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
 
+
+// Fonction pour ajouter l'élément "Admin" dans le menu header à la deuxième position
+function ajouter_item_admin_menu_header($items, $args) {
+    // Vérifier si l'utilisateur est connecté
+    if (is_user_logged_in() && $args->theme_location == 'main-menu') {
+        // Récupérer le lien vers le tableau de bord de l'administration
+        $admin_link = admin_url();
+        // Construire le HTML pour l'élément "Admin"
+        $admin_item = '<li><a href="' . $admin_link . '">Admin</a></li>';
+
+        // Fractionner les éléments du menu en un tableau
+        $menu_items = explode('</li>', $items);
+
+        // Insérer l'élément "Admin" à la deuxième position
+        array_splice($menu_items, 1, 0, $admin_item);
+
+        // Reconstruire la liste des éléments du menu
+        $items = implode('</li>', $menu_items);
+    }
+    return $items;
+}
+// Ajouter la fonction au hook wp_nav_menu_items
+add_filter('wp_nav_menu_items', 'ajouter_item_admin_menu_header', 10, 2);
+
+
 // END ENQUEUE PARENT ACTION
