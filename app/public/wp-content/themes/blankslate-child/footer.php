9a51423cbdfde2d0e4b4ef659a25758b37724a0e
picky37 <149702922+picky37@users.noreply.github.com>
2024-03-01 10:59:46 +0100

    Améliorations responsive + format document

--- app/public/wp-content/themes/blankslate-child/footer.php
+++ app/public/wp-content/themes/blankslate-child/footer.php
@@ -1,12 +1,10 @@
 <footer id="footer" role="contentinfo">
-<!-- <div id="copyright">
-&copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-</div> -->
-<div class="mentions">
-    <p>Mentions Légales</p>
-</div>
+    <div class="mentions">
+        <p>Mentions Légales</p>
+    </div>
 </footer>
 </div>
 <?php wp_footer(); ?>
 </body>
+
 </html>
--- app/public/wp-content/themes/blankslate-child/functions.php
+++ app/public/wp-content/themes/blankslate-child/functions.php
@@ -1,29 +1,32 @@
 <?php
 // Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
 
-if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
-    function chld_thm_cfg_locale_css( $uri ){
-        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+if (!function_exists('chld_thm_cfg_locale_css')) :
+    function chld_thm_cfg_locale_css($uri)
+    {
+        if (empty($uri) && is_rtl() && file_exists(get_template_directory() . '/rtl.css'))
             $uri = get_template_directory_uri() . '/rtl.css';
         return $uri;
     }
 endif;
-add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
+add_filter('locale_stylesheet_uri', 'chld_thm_cfg_locale_css');
 
-if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
-    function chld_thm_cfg_parent_css() {
-        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array(  ) );
+if (!function_exists('chld_thm_cfg_parent_css')) :
+    function chld_thm_cfg_parent_css()
+    {
+        wp_enqueue_style('chld_thm_cfg_parent', trailingslashit(get_template_directory_uri()) . 'style.css', array());
     }
 endif;
-add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
+add_action('wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10);
 
 
 // Fonction pour ajouter l'élément "Admin" dans le menu header à la deuxième position
-function ajouter_item_admin_menu_header($items, $args) {
+function ajouter_item_admin_menu_header($items, $args)
+{
     // Vérifier si l'utilisateur est connecté
     if (is_user_logged_in() && $args->theme_location == 'main-menu') {
         // Récupérer le lien vers le tableau de bord de l'administration
